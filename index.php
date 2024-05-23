9ba3ba44150585b405093e735a1b59e43173b7b1
almsweeee@gmail.com <almsweeee@gmail.com>
2024-05-23 17:06:28 +0500

    no message

--- index.php
+++ index.php
@@ -3,53 +3,27 @@
 	// Code Protect
 	define('ENGINE', true);
 
-	// Определение основных констант 
-	// define('DOMAINS', 'http://jdm-trade/');
-	define('INCDIR', 'inc/');
-	define('TEMPDIR', 'temp/');
-	define('ROUTEDIR', 'routes/');
-	
-	// Запуск сессии пользователя
-	session_start();
-	
-	// Настройка вывода ошибок
-	error_reporting(0);
+		// ob_start();
+		// if($module == 'advantages'){
+		// 	include 'routes/advantages.php';
+		// 	$title = "Наши преимущества";
+		// }elseif($module == 'about'){
+		// 	include 'routes/about.php';
+		// 	$title = "О школе";
+		// }elseif($module == 'feedback'){
+		// 	include 'routes/feedback.php';
+		// 	$title = "Отзывы";
+		// }elseif($module == 'offer'){
+		// 	include 'routes/offer.php';
+		// 	$title = "Мы предлагаем";
+		// }else{
 
-	// PHP info
-	// phpinfo();
-
-	// Подключение библиотеки собственых финкций 
-	// include 'libs/functions.php';
-	
-	// Файл конифигурации базы данных
-	// require INCDIR.'dbconfig.php';
-	
-		// Что такое РОУТИНГ в WEB приложениях
-		$module = $_GET['module'];
-		$title = "";
-		
-		// Подгрузка роута
-		ob_start();
-		if($module == 'advantages'){
-			include 'routes/advantages.php';
-			$title = "Наши преимущества";
-		}elseif($module == 'about'){
-			include 'routes/about.php';
-			$title = "О школе";
-		}elseif($module == 'feedback'){
-			include 'routes/feedback.php';
-			$title = "Отзывы";
-		}elseif($module == 'offer'){
-			include 'routes/offer.php';
-			$title = "Мы предлагаем";
-		}else{
-
-            include 'temp/pageWrapper.php';
-            $title = "Гимназия имени марзии турлыхановой ";
-        }
-		$content = ob_get_contents();
-		ob_end_clean();
-		
+            
+        //     $title = "Гимназия имени марзии турлыхановой ";
+        // }
+		// $content = ob_get_contents();
+		// ob_end_clean();
+		include 'temp/pageWrapper.php';
 
 	// Подключение шаблона
 ?>
